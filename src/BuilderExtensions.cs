f9d6bb41a033b7f3bc430c70adf945b8720bdd38
Mani <manigandham@gmail.com>
2022-04-12 15:53:50 -0700

    split files, fix trimming

--- src/BuilderExtensions.cs
+++ src/BuilderExtensions.cs
@@ -14,8 +14,8 @@ public static class BuilderExtensions
         if (env is null)
             throw new Exception("Cannot resolve IHostEnvironment to get the path to the redirects file.");
 
-        var redirects = ParseFile(Path.Combine(env.ContentRootPath, filePath));
 
+        var redirects = BulkRedirects.ParseFile(Path.Combine(env.ContentRootPath, filePath));
         foreach (var redirect in redirects.Values)
         {
             var permanent = redirect.StatusCode == HttpStatusCode.MovedPermanently;
@@ -28,40 +28,4 @@ public static class BuilderExtensions
 
         return endpoints;
     }
-
-    private static Dictionary<string, Redirect> ParseFile(string filepath)
-    {
-        var redirects = new Dictionary<string, Redirect>();
-        var lines = File.ReadAllLines(filepath);
-
-        foreach (var line in lines)
-        {
-            // skip empty lines and comments
-            if (String.IsNullOrWhiteSpace(line) || line[0] == '#')
-                continue;
-
-            var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Where(p => !string.IsNullOrWhiteSpace(p)).ToArray();
-            if (parts.Length < 2)
-                continue;
-
-            var from = parts[0];
-            var to = parts[1];
-
-            var statusCode = HttpStatusCode.MovedPermanently;
-            if (parts.Length == 3 && parts[2] == "302")
-                statusCode = HttpStatusCode.Found;
-
-            if (!redirects.TryAdd(from, new Redirect { From = from, To = to, StatusCode = statusCode }))
-                throw new Exception($"Redirect from '{from}' has multiple entries.");
-        }
-
-        return redirects;
-    }
-}
-
-internal class Redirect
-{
-    public string From { get; set; } = "";
-    public string To { get; set; } = "";
-    public HttpStatusCode StatusCode { get; set; }
 }
---
+++ src/BulkRedirects.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+
+namespace BulkRedirects;
+
+internal class BulkRedirects
+{
+    public static Dictionary<string, Redirect> ParseFile(string filepath)
+    {
+        var redirects = new Dictionary<string, Redirect>();
+        var lines = File.ReadAllLines(filepath);
+
+        foreach (var line in lines)
+        {
+            // skip empty lines and comments
+            if (String.IsNullOrWhiteSpace(line) || line[0] == '#')
+                continue;
+
+            var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(p => p.Trim()).ToArray();
+            if (parts.Length < 2)
+                continue;
+
+            var from = parts[0];
+            var to = parts[1];
+
+            var statusCode = HttpStatusCode.MovedPermanently;
+            if (parts.Length == 3 && parts[2] == "302")
+                statusCode = HttpStatusCode.Found;
+
+            if (!redirects.TryAdd(from, new Redirect { From = from, To = to, StatusCode = statusCode }))
+                throw new Exception($"Redirect from '{from}' has multiple entries.");
+        }
+
+        return redirects;
+    }
+}
+
+internal class Redirect
+{
+    public string From { get; set; } = "";
+    public string To { get; set; } = "";
+    public HttpStatusCode StatusCode { get; set; }
+}
